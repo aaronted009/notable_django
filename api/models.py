48137cc86197e4d9da85282267153d069d779c94
Aaron <aaronpromail@gmail.com>
2024-05-02 20:23:55 +0100

    Note model definition

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    """`Note` model."""
+
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.title}, {self.body}"
